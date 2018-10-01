bf0b4c916b4c5b72a0ad01618bed9a0ba1c6b31f
koalaboy808 <carlo_liquido@ischool.berkeley.edu>
2018-10-01 14:23:35 -0700

    environment settup and folder redistribution

---
+++ playaround-carlohaven-1.0/src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../style/App.css';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="Homepage">
+        <h1> Our Special Carlo Haven Paired Space </h1>
+        <h6> Love you bu</h6>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
--- playaround-carlohaven-1.0/src/index.js
+++ playaround-carlohaven-1.0/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+
+import './style/index.css';
+import Homepage from './components/Homepage';
+import reducers from './reducers';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={Homepage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+// registerServiceWorker();
